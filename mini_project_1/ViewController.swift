9882081f3b3af063b25cf34d912c15b93a3620b7
FDC-Kyle <fdc.kylev@gmail.com>
2024-07-29 13:51:01 +0800

    Initial Commit

---
+++ mini_project_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mini_project_1
+//
+//  Created by FDC-KYLE-NC-WEB on 7/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
